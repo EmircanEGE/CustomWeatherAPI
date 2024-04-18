789bafdc786fbbb1ad4e4ddcd6e855b8adbc5592
EmircanEGE <ege61emir@gmail.com>
2024-04-19 01:18:29 +0300

    fix weatherservice getweather

--- WeatherApi.Application/Services/WeatherService.cs
+++ WeatherApi.Application/Services/WeatherService.cs
@@ -8,12 +8,13 @@ namespace WeatherApi.Application.Services;
 
 public class WeatherService : IWeatherService
 {
+    private readonly ICityRepository _cityRepository;
+    private readonly IDistrictRepository _districtRepository;
     private readonly IRedisManager _redisManager;
     private readonly IWeatherApiClient _weatherApiClient;
-    private readonly IDistrictRepository _districtRepository;
-    private readonly ICityRepository _cityRepository;
 
-    public WeatherService(IWeatherApiClient weatherApiClient, IRedisManager redisManager, ICityRepository cityRepository, IDistrictRepository districtRepository)
+    public WeatherService(IWeatherApiClient weatherApiClient, IRedisManager redisManager,
+        ICityRepository cityRepository, IDistrictRepository districtRepository)
     {
         _weatherApiClient = weatherApiClient;
         _redisManager = redisManager;
@@ -23,22 +24,19 @@ public class WeatherService : IWeatherService
 
     public async Task<WeatherResponse> GetWeather(string city, int? cityId, int? districtId)
     {
-        string cityName = city;
+        var cityName = city;
         if (cityId != null)
         {
             var dbCity = _cityRepository.Get(x => x.Id == cityId).FirstOrDefault();
             if (dbCity != null)
-            {
                 cityName = dbCity.Name;
-            }
         }
-        else if (districtId != null)
+
+        if (districtId != null)
         {
             var dbDistrict = _districtRepository.Get(x => x.Id == districtId).FirstOrDefault();
             if (dbDistrict != null)
-            {
                 cityName = dbDistrict.Name;
-            }
         }
 
         var cachedResponse = await _redisManager.GetAsync(cityName);
